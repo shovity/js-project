f7206befd7a9105f05ce48c02fbceef674af7ed2
Huong Nguyen <shovity@gmail.com>
2018-08-14 09:14:41 +0700

    add command

--- app.js
+++ app.js
@@ -7,28 +7,38 @@ const fs = require('fs')
 const path = require('path')
 
 // define constants
-const PORT = 80
+const PORT = 3000
 
 // initial apps instance
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
-const slidePathRoot = './slides'
-const slidePaths = fs.readdirSync(slidePathRoot)
+const items = fs.readdirSync('.').map(sp => {
+    const item = {}
+    item.ext = sp.split('.').slice(-1)[0].toLowerCase()
+    item.url = sp
 
-const slideBase64Urls = slidePaths.map(slide_path => {
-    return 'data:image/png;base64,' + fs.readFileSync(path.join(slidePathRoot, slide_path)).toString('base64')    
+    if (['png', 'jpg', 'jpeg', 'gif'].indexOf(item.ext) !== -1) {
+        item.type = 'image'
+    } else if (['mp4', 'mkv', 'flv'].indexOf(item.ext) !== -1) {
+        item.type = 'video'
+    }
+
+    return item
 })
 
 app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.static('.'))
 
 app.get('/', (req, res, next) => {
-    res.render('index', { slideBase64Urls })
+    res.render('index', { items })
 })
 
 app.get('/remote', (req, res, next) => {
-    res.render('remote')
+    res.render('remote', { items })
 })
 
 app.get('/client.js', (req, res, next) => {
--- client.js
+++ client.js
@@ -1,7 +1,10 @@
-const client = io()
-window.slider = slider
+const wH = window.document.documentElement.clientHeight
+const wW = window.document.documentElement.clientWidth
 
-slider.maxSlide = window.maxSlide
+const client = io()
+window.slider = {}
+
+slider.itemsLength = window.itemsLength
 slider.currentIndex = 0
 slider.slideDOMs = document.querySelectorAll('.slide__item')
 
@@ -9,25 +12,58 @@ slider.select = index => {
 	for (let i = 0, l = slider.slideDOMs.length; i < l; i++) {
 		slider.slideDOMs[i].className = slider.slideDOMs[i].className.replace(' slide__item--show', '')
 	}
+
+	if (!slider.slideDOMs[index]) {
+		return console.log('slide dom notfound')
+	}
+
 	slider.slideDOMs[index].className += ' slide__item--show'
 }
 
 client.on('select slide', index => {
+	if (index < 0 || index >= slider.itemsLength) {
+		return console.log('out of slides when seletect')
+	}
 	slider.select(+index)
 })
 
 client.on('next slide', () => {
-	if (slider.currentIndex > slider.maxSlide - 2) {
-		return console.log('out of slides')
+	if (slider.currentIndex > slider.itemsLength - 2) {
+		return console.log('out of slides when mext')
 	}
 	slider.select(++slider.currentIndex)
 })
 
 client.on('prev slide', () => {
-	if (slider.currentIndex > slider.maxSlide - 2) {
-		return console.log('out of slides')
+	if (slider.currentIndex > slider.itemsLength - 2) {
+		return console.log('out of slides when prev')
 	}
 	slider.select(--slider.currentIndex)
 })
 
 slider.select(0)
+
+// client
+
+const threshold = 77
+let start = { x: 0, y: 0}
+
+
+window.controls.addEventListener('touchstart', domEvent => {
+	start.x = domEvent.pageX
+	start.y = domEvent.pageY
+})
+
+window.controls.addEventListener('touchend', domEvent => {
+	domEvent.preventDefault()
+
+	const dV = domEvent.pageX - start.x
+	const dX = domEvent.pageY - start.y
+
+	if (Math.abs(dV) > Math.abs(dX)) {
+		
+	} else {
+
+	}
+
+})
